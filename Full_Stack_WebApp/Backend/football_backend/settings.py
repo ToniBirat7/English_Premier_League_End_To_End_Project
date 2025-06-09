f988dc7437612b4f06dd23f32ef33cfffa746a2b
ToniBirat7 <gotofreelancing1@gmail.com>
2025-06-09 21:47:12 +0545

    Backend Started Project Dir Setup Completed

--- Full_Stack_WebApp/Backend/football_backend/settings.py
+++ Full_Stack_WebApp/Backend/football_backend/settings.py
@@ -37,6 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # Third party apps
+    'rest_framework',
+    'corsheaders',
+    'django_filters',
+    
+    # Local apps
+    'football_api',
 ]
 
 MIDDLEWARE = [
